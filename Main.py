2f3c13f6412f598cf6eae419c6a73caf5e344d1b
T-Rajeev30 <rajeevtiwariunder100@gmail.com>
2024-08-08 20:51:37 +0530

    Calling API to  get JSON data

---
+++ Main.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+api_url = "https://www.planit.org.uk/api/applics/json?auth=Hertfordshire&start_date=2024-07-01&end_date=2024-08-01&"
+
+response = requests.get(api_url)
+data = response.json()
+
+json_path = 'planitData.json'
+with open(json_path, 'w') as json_file:
+    json.dump(data, json_file, indent=4)
+
+print(f"JSON data saved to {json_path}")
